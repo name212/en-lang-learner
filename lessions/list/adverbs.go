a0e5a90732423f657cf89e8b069ed70057dda002
Nikolay Mitrofanov <30695496+name212@users.noreply.github.com>
2025-01-06 11:31:51 +0300

    add adverbs lession (#4)

--- lessions/list/adverbs.go
+++ lessions/list/adverbs.go
@@ -1,6 +1,11 @@
 package list
 
 import (
+	"maps"
+	"math/rand/v2"
+	"slices"
+	"sort"
+
 	"github.com/name212/en-lang-learner/pkg"
 	"github.com/name212/en-lang-learner/pkg/lession"
 )
@@ -9,10 +14,36 @@ type AdverbsLession struct {
 	*lession.BiderictionalRandomLession
 }
 
+func randomElement(m map[string]string) string {
+	keys := slices.Collect(maps.Keys(m))
+	keys = sort.StringSlice(keys)
+	indx := rand.IntN(len(keys))
+	k := keys[indx]
+	return m[k]
+}
+
+var (
+	professions = map[string]string{
+		"teacher":    "учитель",
+		"driver":     "водитель",
+		"painter":    "художник",
+		"model":      "модель",
+		"writer":     "писатель",
+		"backer":     "пекарь",
+		"blogger":    "блоггер",
+		"head":       "глава",
+		"lawyer":     "судья",
+		"loader":     "грузчик",
+		"trainer":    "тренер",
+		"waiter":     "официант",
+		"plumber":    "сантехник",
+		"programmer": "программист",
+	}
+)
+
 func NewAdverbsLession() pkg.Lession {
 	return lession.NewBiderictionalWithExceptionsRandomLession([]pkg.Task{
 		pkg.NewBaseTask("I", "я"),
-		pkg.NewBaseTask("you", "ты"),
 		pkg.NewBaseTask("he", "он"),
 		pkg.NewBaseTask("she", "она"),
 		pkg.NewBaseTask("it", "оно"),
@@ -21,22 +52,40 @@ func NewAdverbsLession() pkg.Lession {
 
 		pkg.NewBaseTask("me", "мне"),
 		pkg.NewBaseTask("him", "ему"),
-		pkg.NewBaseTask("her", "ей"),
 		pkg.NewBaseTask("us", "нам"),
 		pkg.NewBaseTask("them", "им"),
 
-		pkg.NewBaseTask("my", "им"),
-		pkg.NewBaseTask("his", "им"),
-		pkg.NewBaseTask("its", "им"),
-		pkg.NewBaseTask("our", "им"),
-		pkg.NewBaseTask("their", "им"),
+		pkg.NewBaseTask("my", "мой"),
+		pkg.NewBaseTask("his", "его"),
+		pkg.NewBaseTask("its", "его, ёё"),
+		pkg.NewBaseTask("our", "наше"),
+		pkg.NewBaseTask("their", "их"),
 	}, []pkg.Task{
+		pkg.NewBaseTask("ты", "you"),
 		pkg.NewBaseTask("Вы (множ.)", "you"),
 
 		pkg.NewBaseTask("тебе", "you"),
 		pkg.NewBaseTask("Вам", "you"),
-		pkg.NewBaseTask("ему, ей (ср. род)", "you"),
+		pkg.NewBaseTask("ему, ей (ср. род)", "it"),
+		pkg.NewBaseTask("ей", "her"),
 
-		pkg.NewBaseTask("ему, ей (ср. род)", "you"),
+		pkg.NewBaseTask("её", "her"),
+		pkg.NewBaseTask("твой", "your"),
+		pkg.NewBaseTask("ваше", "your"),
+
+		pkg.NewBaseTask("mine", "мой"),
+		pkg.NewBaseTask("hers", "её"),
+		pkg.NewBaseTask("yours", "твой, ваш"),
+		pkg.NewBaseTask("ours", "наш"),
+		pkg.NewBaseTask("theirs", "их"),
+
+		pkg.NewBaseTask("myself", "I"),
+		pkg.NewBaseTask("yourself", "you"),
+		pkg.NewBaseTask("himself", "hе"),
+		pkg.NewBaseTask("herself", "she"),
+		pkg.NewBaseTask("itself", "it"),
+		pkg.NewBaseTask("ourselves", "we"),
+		pkg.NewBaseTask("yourselves", "you"),
+		pkg.NewBaseTask("themselves", "they"),
 	})
 }
